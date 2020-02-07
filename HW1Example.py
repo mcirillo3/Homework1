8cb8ebaee442be6a7d0d8848c9471344c77aa857
mcirillo3 <mcirillo3@fordham.edu>
2020-02-07 02:23:26 -0500

    Homework1

---
+++ HW1Example.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Mark Cirillo
+#Big Data Homework 1 Example
+
+
+# In[1]:
+
+
+text= 'I will use this code as an example for my first homework assignment'
+
+
+# In[ ]:
+
+
+print(text)
+
+
+# In[7]:
+
+
+number=24
+
+
+# In[12]:
+
+
+if number>8:
+    print('The number is greater than 8.')
+else:
+    print('The number is less than or equal to 8.')
+
+
+# In[ ]:
+
+
+
+
